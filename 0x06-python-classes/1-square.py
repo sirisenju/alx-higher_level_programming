a5933926c136d5681f352f6a02c752b46b4d3a03
sirisenju <amrohore4real@gmail.com>
2023-01-24 13:36:51 -0800

    private attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """A class named square"""
+
+    def __init__(self, size):
+        """The constructor of the square class"""
+
+        self.__size = size
