2050a5feee5362e4b4745eef5c1f44554241b1d4
sirisenju <amrohore4real@gmail.com>
2023-01-24 13:26:54 -0800

    initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""empty class square that defines a square"""
+
+class Square:
+    """An empty class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+An introduction to the OOP concepts in python. Creation of classes, its attributes, abstraction and encapsulation.
