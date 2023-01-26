1da51d3c8b99d48968390b7f8b463e2b825a263c
sirisenju <amrohore4real@gmail.com>
2023-01-26 00:25:53 -0800

    divide lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except (TypeError):
             print("wrong trpe")
-            result = 0
+            return 0
         except (ZeroDivisionError):
             print("division by 0")
-            result = 0
+            return 0
         except (IndexError):
             print("out of range")
-            result = 0
+            return 0
         finally:
             new_list.append(result)
     return (new_list)
